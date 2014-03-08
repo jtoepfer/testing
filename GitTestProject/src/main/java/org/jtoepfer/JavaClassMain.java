46ebef1d27729340226f19daf07aca8acdc98a4a
jtoepfer <joshua.toepfer@gmail.com>
2014-03-08 00:22:27 -0600

    Initial commit of changes to local git repository.

---
+++ GitTestProject/src/main/java/org/jtoepfer/JavaClassMain.java
@@ -0,0 +1,8 @@
+package org.jtoepfer;
+
+public class JavaClassMain {
+
+	public static void main(String[] args) {
+		System.out.println("Git is fun.");
+	}
+}
---
+++ GitTestProject/src/test/java/org/jtoepfer/JavaClassMainTest.java
@@ -0,0 +1,5 @@
+package org.jtoepfer;
+
+public class JavaClassMainTest {
+
+}
